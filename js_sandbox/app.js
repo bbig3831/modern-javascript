9e3d8b6602f5bc9161dd75f805f2e478f7e0d565
bbig3831 <beb601@mail.harvard.edu>
2020-08-24 19:27:04 -0400

    Add code for var, const, let section

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -13,3 +13,39 @@ console.warn('This is a warning');
 console.time('Hello');
 
 console.timeEnd('Hello');
+
+// var, let, const
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// Init var
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+// Multi word vars
+var firstName = 'John';
+var first_name = 'Sara';
+var FirstName = 'Tom';
+var firstname;
+
+// CONST
+// const name = 'John';
+console.log(name);
+// Have to assign a value
+// const greeting;
+
+const person = {
+  name: 'John'
+};
+
+console.log(person);
+person.name = 'Sara';
+person.age = 42;
+console.log(person);
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
