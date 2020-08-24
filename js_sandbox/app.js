bb60dca8ec4e415faaa62a980c5746b503f5cc97
bbig3831 <beb601@mail.harvard.edu>
2020-08-24 19:37:43 -0400

    Add code for data types

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,51 +1,82 @@
-// Log to console
-console.log('Hello World');
-console.log(123);
-console.log(true);
-var greeting = 'Hello';
-console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
-console.table({a:1, b:2});
-console.error('This is some error');
-console.clear();
-console.warn('This is a warning');
-console.time('Hello');
+// // Log to console
+// console.log('Hello World');
+// console.log(123);
+// console.log(true);
+// var greeting = 'Hello';
+// console.log(greeting);
+// console.log([1,2,3,4]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
+// console.error('This is some error');
+// console.clear();
+// console.warn('This is a warning');
+// console.time('Hello');
 
-console.timeEnd('Hello');
+// console.timeEnd('Hello');
 
-// var, let, const
-var name = 'John Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
+// // var, let, const
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
 
-// Init var
-var greeting;
-console.log(greeting);
-greeting = 'Hello';
-console.log(greeting);
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
 
-// Multi word vars
-var firstName = 'John';
-var first_name = 'Sara';
-var FirstName = 'Tom';
-var firstname;
+// // Multi word vars
+// var firstName = 'John';
+// var first_name = 'Sara';
+// var FirstName = 'Tom';
+// var firstname;
 
-// CONST
-// const name = 'John';
-console.log(name);
-// Have to assign a value
-// const greeting;
+// // CONST
+// // const name = 'John';
+// console.log(name);
+// // Have to assign a value
+// // const greeting;
 
-const person = {
-  name: 'John'
+// const person = {
+//   name: 'John'
+// };
+
+// console.log(person);
+// person.name = 'Sara';
+// person.age = 42;
+// console.log(person);
+// const numbers = [1,2,3,4,5];
+// numbers.push(6);
+// console.log(numbers);
+
+// Primitives
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+console.log(typeof hasKids);
+console.log(typeof car);
+// Undefined
+let test;
+console.log(typeof test);
+// Symbol
+const sym = Symbol();
+
+console.log(typeof sym);
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+  city: "Boston",
+  state: 'MA'
 };
-
-console.log(person);
-person.name = 'Sara';
-person.age = 42;
-console.log(person);
-const numbers = [1,2,3,4,5];
-numbers.push(6);
-console.log(numbers);
+const today = new Date();
+console.log(today);
+console.log(typeof today);
