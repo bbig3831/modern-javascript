c61f6c62823fa2ed26fc03b1a73a47f4c1ab5689
bbig3831 <beb601@mail.harvard.edu>
2020-08-24 20:45:27 -0400

    Add code for type conversion

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,82 +1,42 @@
-// // Log to console
-// console.log('Hello World');
-// console.log(123);
-// console.log(true);
-// var greeting = 'Hello';
-// console.log(greeting);
-// console.log([1,2,3,4]);
-// console.log({a:1, b:2});
-// console.table({a:1, b:2});
-// console.error('This is some error');
-// console.clear();
-// console.warn('This is a warning');
-// console.time('Hello');
+// Type Conversion
+let val;
 
-// console.timeEnd('Hello');
+// Number to string
+val = 5;
+console.log(val);
+console.log(typeof val);
+val = String(55);
+console.log(val.length);
+// Bool to string
+val = String(true);
+// Date to string
+val = String(new Date());
+// Array to string
+val = String([1,2,3,4]);
 
-// // var, let, const
-// var name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
+// toString()
+val = (5).toString();
+val = (true).toString();
 
-// // Init var
-// var greeting;
-// console.log(greeting);
-// greeting = 'Hello';
-// console.log(greeting);
+// String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val - Number(null);
 
-// // Multi word vars
-// var firstName = 'John';
-// var first_name = 'Sara';
-// var FirstName = 'Tom';
-// var firstname;
+// parseInt();
+val = parseInt('100.30');
+val = parseFloat('100.30');
 
-// // CONST
-// // const name = 'John';
-// console.log(name);
-// // Have to assign a value
-// // const greeting;
+// Output
+console.log(val);
+console.log(typeof val);
+// console.log(val.length);
+console.log(val.toFixed());
 
-// const person = {
-//   name: 'John'
-// };
-
-// console.log(person);
-// person.name = 'Sara';
-// person.age = 42;
-// console.log(person);
-// const numbers = [1,2,3,4,5];
-// numbers.push(6);
-// console.log(numbers);
-
-// Primitives
-// String
-const name = 'John Doe';
-// Number
-const age = 45;
-// Boolean
-const hasKids = true;
-// Null
-const car = null;
-console.log(typeof hasKids);
-console.log(typeof car);
-// Undefined
-let test;
-console.log(typeof test);
-// Symbol
-const sym = Symbol();
-
-console.log(typeof sym);
-
-// REFERENCE TYPES - Objects
-// Array
-const hobbies = ['movies', 'music'];
-// Object literal
-const address = {
-  city: "Boston",
-  state: 'MA'
-};
-const today = new Date();
-console.log(today);
-console.log(typeof today);
+// Type coercion
+const val1 = 5;
+const val2 = '6';
+const sum = val1 + val2;
+console.log(sum);
+console.log(typeof sum);
